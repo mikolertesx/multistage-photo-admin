c24aad0cb189a267fdae43efbf3e0b75352d988e
Miguel Angel <mgangsal@outlook.com>
2026-09-22 16:14:48 -0500

    Create infra file

---
+++ infra/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket       = "terraform-locks-1447"
+    key          = "global/multistage-photo-admin"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
