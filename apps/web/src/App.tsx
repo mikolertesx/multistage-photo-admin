370f382998584d4ecb5458d29fcdf814e358475c
Miguel Angel <mgangsal@outlook.com>
2026-09-23 17:44:19 -0500

    hello world from AWS

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,6 +1,39 @@
+import { useEffect } from 'react'
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { appEnv, siteName } from './env'
 
+const s3 = new S3Client({
+  region: 'us-east-1',
+  requestChecksumCalculation: 'WHEN_REQUIRED',
+  responseChecksumValidation: 'WHEN_REQUIRED',
+  httpAuthSchemeProvider: () => [{ schemeId: 'smithy.api#noAuth' }],
+  httpAuthSchemes: [
+    {
+      schemeId: 'smithy.api#noAuth',
+      identityProvider: () => async () => ({}),
+      signer: { sign: async (request) => request },
+    },
+  ],
+})
+
 function App() {
+  useEffect(() => {
+    s3
+      .send(
+        new GetObjectCommand({
+          Bucket: 'multistage-photo-admin-media-bucket',
+          Key: 'hello.txt',
+        }),
+      )
+      .then((output) => output.Body?.transformToString())
+      .then((text) => {
+        console.log(text)
+      })
+      .catch((error: unknown) => {
+        console.log(error)
+      })
+  }, [])
+
   return (
     <main className="page">
       <p className="env">{appEnv}</p>
