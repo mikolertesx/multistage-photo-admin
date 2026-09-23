9ecb5fc13125a44d8e71a0c10607b017f596f377
Miguel Angel <mgangsal@outlook.com>
2026-09-23 17:26:18 -0500

    create public access S3 bucket

--- infra/main.tf
+++ infra/main.tf
@@ -14,3 +14,45 @@ provider "aws" {
 resource "aws_s3_bucket" "media_bucket" {
   bucket = "multistage-photo-admin-media-bucket"
 }
+
+# This bucket won't be used through CloudFront.
+# IThis is also required for the S3 bucket policy not to be blocked.
+resource "aws_s3_bucket_public_access_block" "allow_public" {
+  bucket = aws_s3_bucket.media_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "allow_external_access" {
+  bucket = aws_s3_bucket.media_bucket.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+
+  depends_on = [ aws_s3_bucket_public_access_block.allow_public ]
+}
+
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.media_bucket.arn,
+      "${aws_s3_bucket.media_bucket.arn}/*",
+    ]
+  }
+}
+
+output "aws_s3_bucket_id" {
+  value = aws_s3_bucket.media_bucket.id
+}
+
